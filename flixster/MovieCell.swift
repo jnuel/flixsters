3f23a2d51ae8c99f895127c530b3cc7b403e904d
jnuel <joynuel@gmail.com>
2020-09-24 20:19:05 -0500

    First commit, working user stories!

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Joy Nuelle on 9/24/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var synopsisLabel: UILabel!
+    @IBOutlet var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
