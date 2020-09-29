fb71f8ba1817a72ef9d600f961c58961943ce556
jnuel <joynuel@gmail.com>
2020-09-29 16:48:11 -0500

    collection view tab loads and displays correctly!

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Joy Nuelle on 9/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet var posterView: UIImageView!
+    
+}
